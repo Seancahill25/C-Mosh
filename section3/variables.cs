985c363f4c2103c4af81f8a4b2dab266fc7f8ecb
Sean Cahill <seancahill25@gmail.com>
2018-05-29 14:42:55 +0000

    shows primitive types of C#

---
+++ section3/variables.cs
@@ -0,0 +1,28 @@
+using System;
+
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Sean";
+            var isWorking = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+        }
+    }
+}
