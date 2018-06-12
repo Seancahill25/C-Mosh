218c522297a0cf5adc0d70988dca9d6f0cd88113
Sean Cahill <seancahill25@gmail.com>
2018-06-12 23:52:04 +0000

    did all looping demos

---
+++ section5/loopDemos/forEach.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace forEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "John Smith";
+
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+
+            var numbers = new int[] {1, 2, 3, 4};
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ section5/loopDemos/whileLoop.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace whileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var i = 0;
+            //while (i <= 10)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
